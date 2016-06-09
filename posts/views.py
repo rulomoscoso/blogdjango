12cc2979d565437400ec380e5319d654f2293f52
Raúl Moscoso <raul.moscoso84@gmail.com>
2016-06-09 18:36:40 -0500

    Template Context

--- posts/views.py
+++ posts/views.py
@@ -7,10 +7,24 @@ def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):#retirve
-	return HttpResponse("<h1>Detail</h1>")
+	context = {
+		"title": "Detail"
+	}
+	return render(request, "index.html", context)
 
 def post_list(request):#list_items
-	return render(request, "index.html", {})
+	context = {
+		"title": "List"
+	}
+	# if request.user.is_authenticated():
+	# 	context = {
+	# 		"title": "My User List"
+	# 	}
+	# else:
+	# 	context = {
+	# 		"title": "List"
+	# 	}
+	return render(request, "index.html", context)
 
 def post_update(request):
 	return HttpResponse("<h1>Update</h1>")
