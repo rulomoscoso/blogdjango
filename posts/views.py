3bf88568723f784c1532428dfe001c67d495694b
Raúl Moscoso <raul.moscoso84@gmail.com>
2016-06-10 18:26:45 -0500

    Dynamic Url Routing & Patterns

--- posts/views.py
+++ posts/views.py
@@ -7,9 +7,9 @@ from .models import Post
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):#retirve
+def post_detail(request, id=None):#retirve
 	#instance = Post.objects.get(id=1)
-	instance = get_object_or_404(Post, id=6)
+	instance = get_object_or_404(Post, id=id)
 	context = {
 		"title": instance.title,
 		"instance": instance,
